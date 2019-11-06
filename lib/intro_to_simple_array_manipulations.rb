75b4c7f9cb584b6e3a93e665e6c6149eb4eb07e0
Daniel Mayle <dmayle012@gmail.com>
2019-11-06 20:13:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,17 @@
 def using_push (array,string)
   array.push(string)
 end
+
+def using_unshift (array,string)
+  array.unshift(string)
+end
+
+def using_pop (array)
+  array.pop()
+end
+
+def using_shift(array)
+  new_arr = array.shift()
+  array.length = array.length -1
+  new_arr
+end
